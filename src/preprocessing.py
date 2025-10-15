f499a4da7c6d014e6700bbf40dc1e3ec093c5f43
Richard <thienrichard005@gmail.com>
2025-10-15 19:41:37 -0400

    removed utils.py and added extra data augmentations

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,11 +5,13 @@ from torchvision import datasets, transforms
 # transformations for train data
 train_transform = transforms.Compose([
     transforms.Resize((48, 48)), # scales photo to 48x48
-    transforms.Grayscale(num_output_channels=1),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(10),
+    transforms.Grayscale(num_output_channels=1), # reduces channel to 1 (grayscale, instead of the 3-channel RGB)
+    transforms.RandomHorizontalFlip(), # horizontal flip (mirror)
+    transforms.RandomRotation(10), # random rotation of up to +/- 10 degrees
     transforms.ColorJitter(brightness=0.3, contrast=0.3), # changes brightness and contrast of a photo sample
-    transforms.ToTensor(), # converts RGB to tensor floats
+    transforms.RandomApply(
+        [transforms.GaussianBlur(kernel_size=(3,3), sigma=(0.05,0.5))], p=0.2), # adds blur, wrapper transform to have a 20% probability
+    transforms.ToTensor(), # converts to tensor floats
     transforms.Normalize(mean=[0.5], std=[0.5]) # normalizing tensor to [-1,1]
 ])
 # transformations for test data (no data augmentations)
