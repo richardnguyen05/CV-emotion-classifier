2d81f4d9885f1a2db1b21047a04d262317d53a56
Richard <thienrichard005@gmail.com>
2025-10-15 19:28:01 -0400

    made it so that figures from visualization are saved in separate folder

--- src/visualization.py
+++ src/visualization.py
@@ -1,9 +1,11 @@
+import os
 import matplotlib.pyplot as plt
 from collections import Counter
+from datetime import datetime
 
 from preprocessing import train_loader, raw_train_loader, train_data, test_data
 
-def plot_samples(images, labels, class_names, n_rows=4, n_cols=4, title="Sample Images"):
+def plot_samples(images, labels, class_names, n_rows=4, n_cols=4, title="Sample Images", save_path=None):
     """
     Plots a grid of images with their corresponding labels
 
@@ -32,6 +34,18 @@ def plot_samples(images, labels, class_names, n_rows=4, n_cols=4, title="Sample 
         ax.set_title(class_names[labels[i]])
         ax.axis('off')
     plt.tight_layout()
+
+    # saving the figures
+    if save_path:
+        # add timestamp to ensure unique file name
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        if save_path.endswith(".png"): # checks if save_path already ends with png (there is already a created figure)
+            full_save_path = save_path.replace(".png", f"_{timestamp}.png") # adding timestamp to ensure no overwriting figures
+        else:
+            full_save_path = f"{save_path}_{timestamp}.png"
+        fig.savefig(full_save_path)
+        print(f"Saved plot to: {full_save_path}")
+    
     plt.show()
 
 def plot_class_distribution(dataset, title="Class Distribution"):
@@ -63,13 +77,14 @@ def plot_class_distribution(dataset, title="Class Distribution"):
 # train data before preprocessing
 print("Visualizing TRAIN DATA before preprocessing...")
 images, labels = next(iter(raw_train_loader))
-plot_samples(images, labels, train_data.classes, title="TRAIN DATA before preprocessing")
+plot_samples(images, labels, train_data.classes, title="TRAIN DATA before preprocessing", save_path="../plots/raw/raw_train_fig")
 
 # train data after preprocessing
 print("Visualizing TRAIN DATA after preprocessing...")
 images, labels = next(iter(train_loader))
-plot_samples(images, labels, train_data.classes, title="TRAIN DATA after preprocessing")
+plot_samples(images, labels, train_data.classes, title="TRAIN DATA after preprocessing", save_path="../plots/preprocessed/preproc_train_fig")
 
+# --- not saving class distribution plot, save_path is empty ---
 # train class distribution
 print("Visualizing TRAIN class distribution...")
 plot_class_distribution(train_data, title="TRAIN Class Distribution")
@@ -77,3 +92,5 @@ plot_class_distribution(train_data, title="TRAIN Class Distribution")
 # test class distribution
 print("Visualizing TEST class distribution...")
 plot_class_distribution(test_data, title="TEST Class Distribution")
+
+print("Visualization complete.")
