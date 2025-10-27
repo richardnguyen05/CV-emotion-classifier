21ae3f7df242e45bb67e966e582567a243b77faa
Richard <thienrichard005@gmail.com>
2025-10-27 19:23:24 -0400

    implemented the test loop

--- src/final_models_results.py
+++ src/final_models_results.py
@@ -3,10 +3,12 @@
 
 import torch
 import matplotlib.pyplot as plt
+import numpy as np
 
-from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 from train_scratch import EmotionCNN, device
 from train_minixception import MiniXception
+from preprocessing import test_loader
 
 # recreating models
 model_minix = MiniXception(num_classes=7).to(device)
@@ -24,3 +26,32 @@ model_scratch.load_state_dict(state_dict)
 
 model_minix.eval()
 model_scratch.eval()
+
+# initializing prediction arrays and true labels
+all_preds_minix = []
+all_preds_scratch = []
+all_labels = []
+
+with torch.no_grad: # disable gradients
+    for images, labels in test_loader:
+        images, labels = images.to(device), labels.to(device)
+
+        # forward pass
+        outputs_minix = model_minix(images)
+        outputs_scratch = model_scratch(images)
+
+        # get predicted classes
+        preds_minix = torch.argmax(outputs_minix, dim=1)
+        preds_scratch = torch.argmax(outputs_scratch, dim=1)
+
+        # store predictions and true labels
+        all_preds_minix.extend(preds_minix.cpu().numpy())
+        all_preds_scratch.extend(preds_scratch.cpu().numpy())
+        all_labels.extend(labels.cpu().numpy())
+
+# convert to numpy arrays
+all_preds_minix = np.array(all_preds_minix)
+all_preds_scratch = np.array(all_preds_scratch)
+all_labels = np.array(all_labels)
+
+# compute metrics
